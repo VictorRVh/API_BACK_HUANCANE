7888e52c57741bc6ecb979913e371bdb71f999d6
Victor Raul <victorraulvaldez54@gmail.com>
2024-09-06 16:43:05 -0700

    db_migraionc

---
+++ database/migrations/2024_09_06_214710_create_usuarios_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->id('id_usuario'); // Clave primaria
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->string('dni')->unique();
+            $table->string('email')->unique();
+            $table->string('telefono')->nullable();
+            $table->string('direccion')->nullable();
+            $table->string('contrasena');
+            $table->unsignedBigInteger('id_rol'); // Clave foránea debe ser bigint
+            $table->foreign('id_rol')->references('id_rol')->on('roles')->onDelete('cascade'); // Definir la relación manualmente
+            $table->timestamps();
+        });
+    }
+
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('usuarios');
+    }
+};
